00a2379971022fa90a9c53d73ea24e8e997c9073
King-Black <kbruening@stud.hs-bremen.de>
2014-05-21 10:03:30 +0200

    try catch hinzugefügt, läuft aber immer noch nicht -.-

--- eShop2.0/src/CUI/CUI.java
+++ eShop2.0/src/CUI/CUI.java
@@ -29,16 +29,21 @@ public class CUI {
 	}
 	
 	public void run() {
-		//shopVer.ladeDaten();
-		//char[] pw = {'1','2','3'};
-		//shopVer.fuegeUserEin("Kunde", pw, "Herr", "Axel Schweiss","Elbenweg 3", 13337, "Bruchtal", "Mittelerde");
-		//shopVer.fuegeUserEin("Mitarbeiter", pw, "Herr", "Voll iDiot");
-		//shopVer.fuegeArtikelEin("EINSTEIN", 1.99, null, 12);
-		gibMenue();
-		//shopVer.speichereDaten();
+		try {
+			//shopVer.ladeDaten();
+//			char[] pw = {'1','2','3'};
+//			shopVer.fuegeUserEin("Kunde", pw, "Herr", "Axel Schweiss","Elbenweg 3", 13337, "Bruchtal", "Mittelerde");
+//			shopVer.fuegeUserEin("Mitarbeiter", pw, "Herr", "Voll iDiot");
+
+//			shopVer.fuegeArtikelEin("EINSTEIN", 1.99, null, 12);
+			gibMenue();
+			//shopVer.speichereDaten();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	private void gibMenue(){
+	private void gibMenue() throws IOException{
 		do{
 			if(!(aktuellerBenutzer == null)){
 				eingeloggt();
@@ -48,7 +53,7 @@ public class CUI {
 		} while (!eingabe.equals("q"));
 	}
 	
-	private void menueNichtEingeloggt(){
+	private void menueNichtEingeloggt() throws IOException {
 		System.out.println("e) Einloggen\n" +
 				"r) Registriere Kunden Account\n" +
 				"q) Beenden");
@@ -58,15 +63,22 @@ public class CUI {
 			String name = liesEingabe();
 			System.out.println("Dein Passwort?");	
 			String passwort = liesEingabe();
-			
-			aktuellerBenutzer = userLogin(name, passwort.toCharArray());
-			
+			try {
+				aktuellerBenutzer = userLogin(name, passwort.toCharArray());
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e);
+			}
 		} else if (eingabe.equals("r")) {
-			userRegistrieren();				
+			try {
+				userRegistrieren();
+			} catch (Exception e) {
+				System.out.println(e);
+			}					
 		}
 	}
 	
-	private void eingeloggt(){
+	private void eingeloggt() throws IOException{
 		System.out.println("Herzlich Willkommen im Shop\n" +
 		"wir wünschen einen angenehmen Aufenthalt\n" +
 		"und ein erfolgreiches Kauferlebnis.\n");
@@ -82,7 +94,7 @@ public class CUI {
 		aktuellerBenutzer = null;
 	}
 	
-	public void userRegistrieren(){
+	public void userRegistrieren() throws IOException{
 		System.out.println("Waehle deinen Benutzernamen:");
 		String name = liesEingabe();
 		String passwort;
@@ -116,7 +128,7 @@ public class CUI {
 		return shopVer.userLogin(name, passwort);
 	}
 	
-	public void menueMitarbeiter(){
+	public void menueMitarbeiter() throws IOException{
 		System.out.println("n) neuen Artikel anlegen \n" +
 				"m) Artikelmenge aendern\n" +
 				"l) Artikel löschen\n" +
@@ -161,19 +173,19 @@ public class CUI {
 		}
 	}
 	
-	private void artikelLoeschen(){
+	private void artikelLoeschen() throws IOException{
 		System.out.println("Welchen Artikel willst du löschen?");
 		int artID = Integer.parseInt(liesEingabe());
 		shopVer.loescheArtikel(artID, aktuellerBenutzer);
 	}
 	
-	private void benutzerLoeschen(){
+	private void benutzerLoeschen() throws IOException{
 		System.out.println("Welchen Mitarbeiter willst du löschen?");
 		int userNr = Integer.parseInt(liesEingabe());
 		shopVer.loescheUser(userNr, aktuellerBenutzer);
 	}
 	
-	private void mitarbeiterErstellen(){
+	private void mitarbeiterErstellen() throws IOException{
 		System.out.println("Waehle deinen Benutzernamen:");
 		String benutzername = liesEingabe();
 		String passwort;
@@ -190,10 +202,15 @@ public class CUI {
 		String vorName = liesEingabe();
 		System.out.println("Nachname:");
 		String nachName = liesEingabe();
-		this.shopVer.fuegeUserEin(benutzername, passwort.toCharArray(), anrede, vorName, nachName);
+		try{	
+			this.shopVer.fuegeUserEin(benutzername, passwort.toCharArray(), anrede, vorName, nachName);
+		}
+		catch(Exception e) {
+			System.out.println(e);				
+		}
 	}
 	
-	private void artikelmengeAendern(){
+	private void artikelmengeAendern() throws IOException{
 		System.out.println("Artikelliste:");
 		gibArtikellisteAus();
 		System.out.println("Artikelnummer des zu ändernden Artikel eingeben.");
@@ -202,11 +219,16 @@ public class CUI {
 		System.out.println("Wieviele moechtest du hinzufügen?");
 		eingabe = liesEingabe();
 		int anzahl = Integer.parseInt(eingabe);
-		shopVer.mengeAendern(nummer, anzahl, aktuellerBenutzer);
+		try{
+			shopVer.mengeAendern(nummer, anzahl, aktuellerBenutzer);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
 		System.out.println("Artikel wurden hinzugefügt!");
 	}
 	
-	private void neuenArtikelAnlegen(){
+	private void neuenArtikelAnlegen() throws IOException{
 		System.out.println("Moechtes du einen Mehrfachartikel speichern? (j für ja und n für nein)");
 		String mehrfach = liesEingabe();
 		int packungsGroesse = 0;
@@ -219,7 +241,8 @@ public class CUI {
 		String beschreibung = liesEingabe();
 		System.out.println("Preis: ");
 		eingabe = liesEingabe();
-		double d = Double.parseDouble(eingabe);	
+		double d = Double.parseDouble(eingabe);
+		try{
 			if (mehrfach.equals("j")) {
 				System.out.println("Bitte gib die Portionsgröße ein.");
 				String portion = liesEingabe();
@@ -229,12 +252,16 @@ public class CUI {
 				shopVer.fuegeArtikelEin(artikelName, menge, beschreibung, d, aktuellerBenutzer);
 	
 			} else {
-				//exception
+				throw new IOException("Bitte entscheide dich für ja oder nein.");
 			}
 			System.out.println("wird angelegt!");
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 	
-	public void menueKunde(){
+	public void menueKunde() throws IOException{
 		System.out.println("w) Zum Warenkorb\n" +
 				"m) Artikelmenge im Warenkorb ändern\n" +
 				//"b) Artikelbeschreibung aufrufen\n" +
@@ -259,7 +286,12 @@ public class CUI {
 				System.out.println("Wieviel möchtest du hinzufügen oder abziehen?");
 				eingabe = liesEingabe();
 				int menge = Integer.parseInt(eingabe);
-				shopVer.artikelMengeImWarenkorbAendern(artID, menge, (Kunde)aktuellerBenutzer);
+				try{
+					shopVer.artikelMengeImWarenkorbAendern(artID, menge, (Kunde)aktuellerBenutzer);
+				}
+				catch (Exception e) {
+					System.out.println(e);
+				}
 				//gibArtikellisteAus();
 				break;
 //			case "b": 
@@ -285,10 +317,20 @@ public class CUI {
 				System.out.println("Welchen Artikel entfernen?");
 				eingabe = liesEingabe();
 				artID = Integer.parseInt(eingabe);
-				shopVer.artikelAusWarenkorb(artID, (Kunde)aktuellerBenutzer);
+				try{
+					shopVer.artikelAusWarenkorb(artID, (Kunde)aktuellerBenutzer);
+				}
+				catch (Exception e) {
+					System.out.println(e);
+				}
 				break;
 			case "e":
-				shopVer.warenkorbLeeren((Kunde)aktuellerBenutzer);
+				try{
+					shopVer.warenkorbLeeren((Kunde)aktuellerBenutzer);
+				}
+				catch (Exception e) {
+					System.out.println(e);
+				}
 				break;
 			case "k": zurKasse();
 				break;
@@ -311,7 +353,7 @@ public class CUI {
 		}
 	}
 	
-	private String liesEingabe(){
+	private String liesEingabe() throws IOException{
 		return in.readLine();
 	}
 
