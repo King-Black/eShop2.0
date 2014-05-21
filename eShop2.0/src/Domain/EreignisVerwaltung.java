a495a90813abb77eee3b02c1561206e309bf738a
King-Black <kbruening@stud.hs-bremen.de>
2014-05-21 13:24:35 +0200

    ereignisverwaltung und ereignisklasse befüllt

--- eShop2.0/src/Domain/EreignisVerwaltung.java
+++ eShop2.0/src/Domain/EreignisVerwaltung.java
@@ -1,33 +1,67 @@
 package Domain;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 
 import Valueobjects.Artikel;
 import Valueobjects.Ereignis;
-import Valueobjects.Kunde;
 import Valueobjects.User;
 
 public class EreignisVerwaltung {
+	
+	private List<Ereignis> protokoll = new Vector<Ereignis>();
 
-	public void ereignisEinfuegen(User akteur, Artikel a, int artikelBestand,
-			String string) {
-		// TODO Auto-generated method stub
-		
+	public void ereignisEinfuegen(User akteur, Artikel derWars, int artikelBestand, String aktion) {
+		Ereignis ereignis = new Ereignis(akteur, derWars, artikelBestand, aktion);
+		protokoll.add(ereignis);
 	}
 
 	public void gibProtokollAus() {
-		// TODO Auto-generated method stub
+		if(protokoll.isEmpty()) {
+			System.out.println("Liste ist leer.");
+		} else {
+			Iterator<Ereignis> it = protokoll.iterator();
+			while (it.hasNext()) {
+				Ereignis ereignis = it.next();
+				System.out.println(ereignis.toString());
+			}			
+		}
+		System.out.println(" ");
+	}
+	
+	public List<Ereignis> gibEreignisseNachArtikelUndTagen(Artikel a) {
+		// anzahl tage wird nicht benutzt
+			List<Ereignis> liste = new Vector<Ereignis>();
+
+			Calendar heute = Calendar.getInstance();
+			Calendar ereignis = new GregorianCalendar();		
+			
+			// hier werden alle ereignisse aus protokoll durchgegangen
+			for(Ereignis e:protokoll) {
+				ereignis.setTime(e.getDate());                      // zweiter Zeitpunkt
+				long zeitVergangen = ereignis.getTime().getTime() - heute.getTime().getTime();  // Differenz in ms
+				long inTagen = Math.round( (double)zeitVergangen / (24. * 60.*60.*1000.) ); // Zeit Differenz in Tagen
+				// wenn ereignis in dem zeitraum liegt, in die liste
+				// bedingung bewirkt das nichts, das laenger als 30 tage zurueck liegt in die liste gepackt wird
+				if(inTagen>30){
+					return liste;
+				} else {
+					if(e.getArtikel().getArtikelNummer()==a.getArtikelNummer()) {
+						liste.add(e);
+					} 
+				}
+			}	
+			return liste;
+				
 		
 	}
-
+	
 	public List<Ereignis> gibProtokollListe() {
-		// TODO Auto-generated method stub
-		return null;
+		return protokoll;
 	}
 
-	public List<Ereignis> gibEreignisseNachArtikelUndTagen(Artikel a) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
--- eShop2.0/src/Valueobjects/Ereignis.java
+++ eShop2.0/src/Valueobjects/Ereignis.java
@@ -1,5 +1,45 @@
 package Valueobjects;
 
+import java.util.Date;
+
 public class Ereignis {
+	
+	private User akteur;
+	private Date datum;
+	private Artikel artikel;
+	private String aktion;
+	private int anzahl;
+	
+	public Ereignis(User akteur, Artikel artikel, int anzahl	, String aktion) {
+		this.akteur = akteur;
+		this.datum = new Date();
+		this.artikel = artikel;
+		this.anzahl = anzahl;
+		this.aktion = aktion;
+	}
+	
+	public Artikel getArtikel() {
+		return this.artikel;
+	}
+	
+	public Date getDate() {
+		return this.datum;
+	}
+	
+	public User getUser() {
+		return akteur;
+	}
+	
+	public int getMenge() {
+		return anzahl;
+	}
+	
+	public String getAktion() {
+		return aktion;
+	}
+	
+	public String toString() {
+		return ("Datum: " + this.datum + " User: " + akteur + " Artikel: " + artikel.getArtikelName() + " | Anzahl: " + anzahl + " | " + " Aktion: " + aktion);
+	}
 
 }
