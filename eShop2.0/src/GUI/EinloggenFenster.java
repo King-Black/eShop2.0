6885be5b22775ffa46d2ae8a78282ca860699a46
Leokina <leokina@hotmail.de>
2014-06-04 11:25:07 +0200

    erstmal so aus altem entnommen, muss noch umgeschrieben werden.

--- eShop2.0/src/GUI/EinloggenFenster.java
+++ eShop2.0/src/GUI/EinloggenFenster.java
@@ -1,5 +1,185 @@
 package GUI;
 
-public class EinloggenFenster {
+import java.awt.GridLayout;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.text.NumberFormat;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+
+import exceptions.BereitsEingeloggtException;
+import exceptions.KennwortFalschException;
+import exceptions.PersonIdNichtGefundenException;
+import exceptions.PersonTypNichtGefundenException;
+
+/**
+ * Klasse zur Erstellung eines Login-Fensters.
+ *
+ */
+@SuppressWarnings("serial")
+public class EinloggenFenster extends JDialog {
+	private JButton okButton;
+	private JButton abbrechenButton;
+	private JPasswordField passwortText;
+	private JFormattedTextField idText;
+	private JComboBox<String> personTypComboBox;
+	private boolean erfolg = false;
+	final EinloggenFenster gui = this;
+	
+	/**
+	 * Initialisiert ein modales Login-Fenster mit fester Größe einem vorgegebenen Hauptfenster.
+	 * @param owner Das Hauptfenster, des Login-Fensters.
+	 */
+	public EinloggenFenster(Window owner) {
+		super(owner, "Einloggen", ModalityType.APPLICATION_MODAL);
+		
+		this.setResizable(false);
+		this.initialisieren();
+	}
+	
+	/**
+	 * Initialisiert das Login-Fenster mit allen Komponenten.
+	 */
+	public void initialisieren() {
+		//Layout setzen und Variabeln initialisieren:
+		this.setLayout(new GridLayout(4, 2, 6, 3));
+		personTypComboBox = new JComboBox<String>(new String[] {"Kunde", "Mitarbeiter"});
+		idText = new JFormattedTextField(NumberFormat.getIntegerInstance());
+		passwortText = new JPasswordField();
+		okButton = new JButton("OK");
+		abbrechenButton = new JButton("Abbrechen");
+		
+		//Komponenten ins Layout einfügen:
+		this.add(new JLabel(""));
+		this.add(personTypComboBox);
+		this.add(new JLabel("Benutzer-ID:"));
+		this.add(idText);
+		this.add(new JLabel("Passwort:"));
+		this.add(passwortText);
+		this.add(okButton);
+		this.add(abbrechenButton);
+		
+		//ActionListener für den Abbrechen-Button hinzufügen:
+		ActionListener abort = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				dispose();
+	        }
+	    };
+	    
+	    //Action- und KeyListener zu den Buttons bzw. dem Passwort-Feld hinzufügen:
+	    okButton.addActionListener(this.okListener());
+	    abbrechenButton.addActionListener(abort);
+	    passwortText.addKeyListener(this.enterListener());
+		
+	    //Fenster auf optimale Größe packen:
+		this.pack();
+	}
+	
+	/**
+	 * Die Methode gibt bei erfolgreichem Einloggen true zurück, andernfalls false.
+	 * @return Gibt bei erfolgreichem Einloggen true zurück, andernfalls false.
+	 */
+	public boolean getErfolg(){
+		return this.erfolg;
+	}
+	
+	/**
+	 * Die Methode implementiert einen KeyListener, mit welchem man sich per Enter-Taste in den EShop einloggen kann 
+	 * und gibt diesen zurück.
+	 * @return Gibt den KeyListener für die Enter-Taste zurück.
+	 */
+	private KeyListener enterListener(){
+		KeyListener enter = new KeyListener(){
+			@Override
+			public void keyPressed(KeyEvent e){
+				//lässt den Nutzer seine Eingab auch per Enter-Taste bestätigen und loggt ihn ein:
+				if (e.getKeyCode() == KeyEvent.VK_ENTER){
+					try {
+						HauptFenster.benutzer = HauptFenster.shopVerwaltung.einloggen(Integer.parseInt(gui.idText.getText()),
+								gui.personTypComboBox.getSelectedIndex(), String.valueOf(gui.passwortText.getPassword()));
+						erfolg = true;
+						gui.dispose();
+					} catch (NumberFormatException e1) {
+						JOptionPane dialog = new JOptionPane();
+						JOptionPane.showMessageDialog(EinloggenFenster.this, "Bitte geben Sie eine Zahl ein.", "Error", JOptionPane.ERROR_MESSAGE);
+						dialog.setVisible(true);
+					} catch (PersonIdNichtGefundenException e1) {
+						JOptionPane dialog = new JOptionPane();
+						JOptionPane.showMessageDialog(EinloggenFenster.this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+						dialog.setVisible(true);
+					} catch (PersonTypNichtGefundenException e1) {
+						JOptionPane dialog = new JOptionPane();
+						JOptionPane.showMessageDialog(EinloggenFenster.this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+						dialog.setVisible(true);
+					} catch (KennwortFalschException e1) {
+						JOptionPane dialog = new JOptionPane();
+						JOptionPane.showMessageDialog(EinloggenFenster.this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+						dialog.setVisible(true);
+					} catch (BereitsEingeloggtException e1) {
+						JOptionPane dialog = new JOptionPane();
+						JOptionPane.showMessageDialog(EinloggenFenster.this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+						dialog.setVisible(true);
+					}				
+				}
+			}
+			
+			//Methoden, die im KeyListener enthalten sind, aber von uns nicht benötigt werden:
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+			}
+		};
+		return enter;
+	}
+
+	/**
+	 * Die Methode implementiert den ActionListener für den Ok-Button und gibt zurück.
+	 * @return Gibt den ActionListener zurück.
+	 */
+	private ActionListener okListener(){
+		ActionListener ok = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//loggt den Benutzer per Klick auf den OK-Button ein und gibt ggf. Fehlermeldungen aus:
+				try {
+					HauptFenster.benutzer = HauptFenster.eShopVerwaltung.einloggen(Integer.parseInt(gui.idText.getText()),
+							gui.personTypComboBox.getSelectedIndex(), String.valueOf(gui.passwortText.getPassword()));
+					erfolg = true;
+					gui.dispose();
+				} catch (NumberFormatException e1) {
+					JOptionPane dialog = new JOptionPane();
+					JOptionPane.showMessageDialog(EinloggenFenster.this, "Bitte geben Sie eine Zahl ein.", "Error", JOptionPane.ERROR_MESSAGE);
+					dialog.setVisible(true);
+				} catch (PersonIdNichtGefundenException e1) {
+					JOptionPane dialog = new JOptionPane();
+					JOptionPane.showMessageDialog(EinloggenFenster.this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					dialog.setVisible(true);
+				} catch (PersonTypNichtGefundenException e1) {
+					JOptionPane dialog = new JOptionPane();
+					JOptionPane.showMessageDialog(EinloggenFenster.this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					dialog.setVisible(true);
+				} catch (KennwortFalschException e1) {
+					JOptionPane dialog = new JOptionPane();
+					JOptionPane.showMessageDialog(EinloggenFenster.this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					dialog.setVisible(true);
+				} catch (BereitsEingeloggtException e1) {
+					JOptionPane dialog = new JOptionPane();
+					JOptionPane.showMessageDialog(EinloggenFenster.this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					dialog.setVisible(true);
+				}				
+			}
+		};
+		return ok;
+	}
 }
--- eShop2.0/src/GUI/VerwaltungsPanel.java
+++ eShop2.0/src/GUI/VerwaltungsPanel.java
@@ -1,5 +1,250 @@
 package GUI;
 
-public class VerwaltungsPanel {
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import exceptions.NichtEingeloggtException;
+import Valueobjects.Kunde;
+import Valueobjects.Mitarbeiter;
+
+/**
+ * Klasse zur Erstellung des VerwaltungsPanels.
+ *
+ */
+@SuppressWarnings("serial")
+public class VerwaltungsPanel extends JPanel {
+	private JPanel verwaltungsPanel;
+	private JButton einloggenButton;
+	private JButton ausloggenButton;
+	private HauptFenster hauptFenster;
+	private WarenkorbPanel warenkorbPanel;
+	private ArtikelPanel artikelPanel;
+	private LagerEreignisPanel lagerPanel;
+	private JButton registrierenButton;
+	private JButton adresseAendernButton;
+	private JButton mitarbeiterAnlegenButton;
+	
+	/**
+	 * Initialisiert ein VerwaltunsPanel-Objekt.
+	 * @param hf Das Hauptfenster der GUI.
+	 * @param wp Das WarenkorbPanel der GUI.
+	 * @param ap Das ArtikelPanel der GUI.
+	 * @param lep Das LagerEreignisPanel der GUI.
+	 */
+	public VerwaltungsPanel(HauptFenster hf, WarenkorbPanel wp, ArtikelPanel ap, LagerEreignisPanel lep){
+		super();
+		
+		this.hauptFenster = hf;
+		this.warenkorbPanel = wp;
+		this.artikelPanel = ap;
+		this.lagerPanel = lep;
+		
+		this.initialisieren();
+	}
+	
+	/**
+	 * Die Methode initialisiert die Komponenten des VerwaltungsPanel.
+	 */
+	private void initialisieren(){
+		//Komponenten des VerwaltungsPanel initialisieren:
+		verwaltungsPanel = new JPanel();
+		einloggenButton = new JButton("Einloggen");
+		ausloggenButton = new JButton("Ausloggen");
+		registrierenButton = new JButton("Registrieren");
+		adresseAendernButton = new JButton("Adresse ändern");
+		mitarbeiterAnlegenButton = new JButton("Mitarbeiter anlegen");
+		
+	    //ActionListener der Buttons hinzufügen und Sichtbarkeit setzen:
+		einloggenButton.addActionListener(this.einloggen());
+		einloggenButton.setVisible(true);
+
+		ausloggenButton.addActionListener(this.ausloggen());
+		ausloggenButton.setVisible(false);
+		
+		registrierenButton.addActionListener(this.registrieren());
+		registrierenButton.setVisible(true);
+		
+		adresseAendernButton.addActionListener(this.adresseAendern());
+		adresseAendernButton.setVisible(false);
+		
+		mitarbeiterAnlegenButton.addActionListener(this.mitarbeiterAnlegen());
+		mitarbeiterAnlegenButton.setVisible(false);
+		
+		//fügt die Buttons zum VerwaltungsPanel hinzu:
+		verwaltungsPanel.add(ausloggenButton);
+		verwaltungsPanel.add(einloggenButton);
+		verwaltungsPanel.add(registrierenButton);
+		verwaltungsPanel.add(adresseAendernButton);
+		verwaltungsPanel.add(mitarbeiterAnlegenButton);
+		
+		//fügt das VerwaltungsPanelPanel in das Layout ein:
+		this.add(verwaltungsPanel, BorderLayout.NORTH);
+	}
+	
+	/**
+	 * Die Methode implementiert den ActionListener für den Einloggen Button und gibt diesen zurück.
+	 * @return Gibt den ActionListener für den Einloggen Button zurück.
+	 */
+	private ActionListener einloggen(){
+		ActionListener einloggen = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//erstellt ein neues Einloggen Fenster:
+				EinloggenFenster login = new EinloggenFenster(hauptFenster);
+				login.setLocationRelativeTo(hauptFenster);
+				login.setVisible(true);
+				//bei erfolgreichem Login ausführen:
+				if(login.getErfolg()) {
+					hauptFenster.setTitle("EShop - " + HauptFenster.benutzer.getVorName() +
+							" " + HauptFenster.benutzer.getNachName());
+					einloggenButton.setVisible(false);
+					ausloggenButton.setVisible(true);
+					registrierenButton.setVisible(false);
+					//falls der eingeloggte Benutzer ein Kunde ist:
+					if(HauptFenster.benutzer != null && HauptFenster.benutzer instanceof Kunde){
+						warenkorbPanel = new WarenkorbPanel(VerwaltungsPanel.this.hauptFenster);
+						hauptFenster.tabsPanel.addTab("Warenkorb", warenkorbPanel);
+						adresseAendernButton.setVisible(true);
+						hauptFenster.pack();
+					}
+					//falls der eingeloggte Benutzer ein Mitarbeiter ist:
+					if(HauptFenster.benutzer != null && HauptFenster.benutzer instanceof Mitarbeiter){
+						artikelPanel.inDenWarenkorbButton.setVisible(false);
+						artikelPanel.add(artikelPanel.getSouthPanel(), BorderLayout.SOUTH);
+						mitarbeiterAnlegenButton.setVisible(true);
+						hauptFenster.tabsPanel.addTab("Lagerereignisse", lagerPanel);
+						hauptFenster.pack();
+					}
+				}else{
+					einloggenButton.setVisible(true);
+				}
+	        }
+	    };
+	    return einloggen;
+	}
+	
+	/**
+	 * Die Methode implementiert den ActionListener für den Ausloggen Button und gibt diesen zurück.
+	 * @return Gibt den ActionListener für den Ausloggen Button zurück.
+	 */
+	private ActionListener ausloggen(){
+		ActionListener ausloggen = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//versetzt das VerwaltungsPanel wieder in den Ursprungszustand:
+				JOptionPane logout = new JOptionPane();
+				JOptionPane.showMessageDialog(hauptFenster, "Sie haben sich erfolgreich abgemeldet!",
+						"Logout", JOptionPane.INFORMATION_MESSAGE);
+				logout.setVisible(true);
+				ausloggenButton.setVisible(false);
+				einloggenButton.setVisible(true);
+				registrierenButton.setVisible(true);
+				hauptFenster.setTitle("EShop");
+				hauptFenster.tabsPanel.remove(1);
+				//falls der noch(!) eingeloggte Benutzer ein Mitarbeiter ist:
+				if(HauptFenster.benutzer != null && HauptFenster.benutzer instanceof Mitarbeiter){
+					artikelPanel.remove(2);
+					mitarbeiterAnlegenButton.setVisible(false);
+				}
+				//falls der noch(!) eingeloggte Benutzer ein Kunde ist:
+				if(HauptFenster.benutzer != null && HauptFenster.benutzer instanceof Kunde){
+					adresseAendernButton.setVisible(false);
+				}
+				hauptFenster.pack();
+				//loggt den Benutzer aus und setzt den Benutzer des Hauptfensters auf null:
+				try {
+					HauptFenster.eShopVerwaltung.ausloggen(HauptFenster.benutzer);
+					HauptFenster.benutzer = null;
+				} catch (NichtEingeloggtException e1) {
+					JOptionPane dialog = new JOptionPane();
+					JOptionPane.showMessageDialog(VerwaltungsPanel.this, "Sie müssen sich erst einloggen, bevor Sie sich ausloggen können.", "Error", JOptionPane.ERROR_MESSAGE);
+					dialog.setVisible(true);
+				}
+			}
+		};
+		return ausloggen;
+	}
+
+	/**
+	 * Die Methode implementiert den ActionListener für den Registrieren Button und gibt diesen zurück.
+	 * @return Gibt den ActionListener für den RegistrierenButton zurück.
+	 */
+	private ActionListener registrieren(){
+		ActionListener registrieren = new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//erzeugt ein neues RegistrierenFenster und übergibt diesem die Aktion:
+				RegistrierenFenster registrierenFenster = new RegistrierenFenster(hauptFenster, 0);
+				registrierenFenster.add(new JLabel("Nur Kunden können sich selbst registrieren!"), BorderLayout.NORTH);
+				
+				//stellt die benötigten Komponenten des RegistrierenFensters zusammen:
+				JPanel panel = new JPanel();
+				panel.setLayout(new GridLayout(2, 1, 6, 3));
+				panel.add(registrierenFenster.getRegistrierenPanel());
+				panel.add(registrierenFenster.getAdressPanel());
+				registrierenFenster.add(panel, BorderLayout.CENTER);
+				registrierenFenster.pack();
+				registrierenFenster.setVisible(true);
+				registrierenFenster.setLocationRelativeTo(hauptFenster);
+			}
+		};
+		return registrieren;
+	}
+	
+	/**
+	 * Die Methode implementiert den ActionListener für den Adresse ändern Button und gibt diesen zurück.
+	 * @return Gibt den ActionListener für den Adresse ändern Button zurück.
+	 */
+	private ActionListener adresseAendern() {
+		ActionListener adresseAendern = new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//erzeugt ein neues RegistrierenFenster mit der übergebenen Aktion 1:
+				RegistrierenFenster registrierenFenster = new RegistrierenFenster(hauptFenster, 1);
+				//prüft, ob der eingeloggte Benutzer ein Kunde ist und stellt die Komponenten
+				//des RegistrierenFensters zusammen:
+				if(HauptFenster.benutzer != null && HauptFenster.benutzer instanceof Kunde){
+					Kunde k = (Kunde)HauptFenster.benutzer;
+					registrierenFenster.setTitle("Adresse ändern");
+					registrierenFenster.add(registrierenFenster.getAktuelleAdressePanel(), BorderLayout.NORTH);
+					registrierenFenster.aktuelleAdresseText.setText(k.getAdresse().getStrasse() + "\n"
+							+ k.getAdresse().getPLZ() + " " + k.getAdresse().getStadt());
+					registrierenFenster.add(registrierenFenster.getAdressPanel(), BorderLayout.CENTER);
+				}
+				registrierenFenster.pack();
+				registrierenFenster.setVisible(true);
+				registrierenFenster.setLocationRelativeTo(hauptFenster);
+			}
+		};
+		return adresseAendern;
+	}
+	
+	/**
+	 * Die Methode implementiert den ActionListener für den Mitarbeiter anlegen Button und gibt diesen zurück.
+	 * @return Gibt den ActionListener für den Mitarbeiter anlegen Button zurück.
+	 */
+	private ActionListener mitarbeiterAnlegen() {
+		ActionListener mitarbeiterAnlegen = new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//erzeugt ein neues RegistrierenFenster mit der übergebenen Aktion 2:
+				RegistrierenFenster registrierenFenster = new RegistrierenFenster(hauptFenster, 2);
+				//prüft, ob der eingeloggte Benutzer ein Mitarbeiter ist und stellt die Komponenten
+				//des RegistrierenFensters zusammen:
+				if(HauptFenster.benutzer != null && HauptFenster.benutzer instanceof Mitarbeiter){
+					registrierenFenster.setTitle("Mitarbeiter anlegen");
+					registrierenFenster.add(registrierenFenster.getRegistrierenPanel());
+				}
+				registrierenFenster.pack();
+				registrierenFenster.setVisible(true);
+				registrierenFenster.setLocationRelativeTo(hauptFenster);
+			}
+		};
+		return mitarbeiterAnlegen;
+	}
 }
