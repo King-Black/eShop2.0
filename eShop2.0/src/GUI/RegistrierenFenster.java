c6b8921274134eaf86fa8a9bf5942452d31c2871
Leokina <Leokina@Leokina-PC>
2014-07-08 18:04:45 +0200

    neue GUI Fenster

--- eShop2.0/src/GUI/RegistrierenFenster.java
+++ eShop2.0/src/GUI/RegistrierenFenster.java
@@ -1,12 +1,359 @@
 package GUI;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+import exceptions.BereitsVorhandenException;
+import exceptions.KonnteNichtSpeichernException;
+import Valueobjects.Adresse;
+import Valueobjects.Kunde;
+import Valueobjects.Mitarbeiter;
+
+/**
+ * Klasse zur Erstellung des RegistrierenFensters.
+ *
+ */
 @SuppressWarnings("serial")
-public class RegistrierenFenster extends JFrame{
-
-	public RegistrierenFenster() {
-		// TODO Auto-generated constructor stub
+public class RegistrierenFenster extends JDialog {
+	private JPanel registrierenPanel;
+	private JButton okButton;
+	private JButton abbrechenButton;
+	private JTextField vorNameText;
+	private JTextField nachNameText;
+	private JTextField kennwortText;
+	private JPanel adressPanel;
+	private JTextField strasseText;
+	private JTextField plzText;
+	private JTextField stadtText;
+	private JPanel buttonPanel;
+	private JPanel aktuelleAdressePanel;
+	JTextArea aktuelleAdresseText;
+	private int aktion;
+	
+	/**
+	 * Initialisiert ein RegistrierenFenster-Objekt, welches je nach gewählter Aktion sein Aussehen ändert
+	 * und standardmäßig nur Ok- und Abbrechen-Button beinhaltet.
+	 * @param owner Das Hauptfenster des RegistrierenFensters.
+	 * @param aktion Die Aktion, die ausgeführt werden soll. 0 für Kunden registrieren, 1 für Adresse ändern
+	 * und 2 für Mitarbeiter registrieren.
+	 */
+	public RegistrierenFenster(Window owner, int aktion){
+		super(owner, "Registrieren");
+		this.aktion = aktion;
+		
+		this.setLayout(new BorderLayout());
+	
+		this.buttonPanelErzeugen();
+		this.setResizable(false);
+		this.pack();
 	}
+	
+	/**
+	 * Die Methode erzeugt das RegistrierenPanel, welches Vor- und Nachname und das neue Passwort einliest
+	 * und gibt dieses zurück.
+	 * @return Gibt das RegistrierenPanel zurück.
+	 */
+	private JPanel registrierenPanelErzeugen(){
+		//initialisiert alle Komponenten des RegistrierenPanel und setzt das Layout:
+		registrierenPanel = new JPanel();
+		registrierenPanel.setLayout(new GridLayout(3, 2, 6, 3));
+		vorNameText = new JTextField();
+		nachNameText = new JTextField();
+		kennwortText = new JTextField();
+		
+		//fügt alle Komponenten in das Layout ein:
+	    registrierenPanel.add(new JLabel("Vorname:"));
+	    registrierenPanel.add(vorNameText);
+	    registrierenPanel.add(new JLabel("Nachname:"));
+	    registrierenPanel.add(nachNameText);
+	    registrierenPanel.add(new JLabel("Passwort:"));
+	    registrierenPanel.add(kennwortText);
+	    
+	    return registrierenPanel;
+	}
+	
+	/**
+	 * Die Methode erzeugt das AdressPanel, welches Straße, Postleitzahl und Stadt einliest
+	 * und das Panel zurückgibt.
+	 * @return Gibt das AdressPanel zurück.
+	 */
+	private JPanel adressPanelErzeugen(){
+		//initialisiert alle Komponenten des AdressPanels und setzt das Layout:
+		adressPanel = new JPanel();
+		adressPanel.setLayout(new GridLayout(3, 2, 6, 3));
+		strasseText = new JTextField();
+		plzText = new JTextField();
+		stadtText = new JTextField();
+		
+		//fügt alle Komponenten in das Layout ein:
+		adressPanel.add(new JLabel("Straße und Hausnummer:"));
+		adressPanel.add(strasseText);
+		adressPanel.add(new JLabel("Postleitzahl:"));
+		adressPanel.add(plzText);
+		adressPanel.add(new JLabel("Stadt:"));
+		adressPanel.add(stadtText);
+		
+		return adressPanel;
+	}
+	
+	/**
+	 * Die Methode erzeugt das Panel mit der aktuellen Adresse, welche bei der Adressänderung ausgegeben wird
+	 * und gibt das Panel zurück.
+	 * @return Gibt das aktuelleAdressePanel zurück.
+	 */
+	private JPanel aktuelleAdressePanelErzeugen(){
+		//initialisiert alle Komponenten des Panels:
+		aktuelleAdressePanel = new JPanel();
+		aktuelleAdresseText = new JTextArea();
+		
+		//definiert das Textfeld genau:
+		TitledBorder tBorderBeschreibung = BorderFactory.createTitledBorder("Aktuelle Adresse");
+		aktuelleAdresseText.setBorder(tBorderBeschreibung);
+		aktuelleAdresseText.setEditable(false);
+		aktuelleAdresseText.setWrapStyleWord(true);
+		aktuelleAdresseText.setLineWrap(true);
+		aktuelleAdresseText.setOpaque(false);
+		aktuelleAdresseText.setFont(new JLabel().getFont());
+		
+		//fügt das Textfeld in das Panel ein:
+		aktuelleAdressePanel.add(aktuelleAdresseText);
+		
+		return aktuelleAdressePanel;
+	}
+	
+	/**
+	 * Die Methode erzeugt das ButtonPanel.
+	 */
+	private void buttonPanelErzeugen(){
+		//initialisiert alle Komponenten des ButtonPanel:
+		buttonPanel = new JPanel();
+		okButton = new JButton("Ok");
+		abbrechenButton = new JButton("Abbrechen");
+		
+		//implementiert den ActionListener für den Abbrechen Button:
+		ActionListener abort = new ActionListener(){
+	
+			public void actionPerformed(ActionEvent e){
+				dispose();
+	        }
+	    };
+	    //fügt die ActionListener zu den Buttons hinzu:
+	    abbrechenButton.addActionListener(abort);
+	    okButton.addActionListener(this.okListener());
+		
+	    //fügt die Buttons in das Panel ein:
+		buttonPanel.add(okButton);
+		buttonPanel.add(abbrechenButton);
+		
+		//fügt das Panel in das Layout ein:
+		this.add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	/**
+	 * Die Methode implementiert den ActionListener für den Ok-Button und gibt diesen zurück.
+	 * Die Funktionalität ändert sich je nach der im Konstruktor übergebenen Aktion.
+	 * @return Gibt den ActionListener für den Ok-Button zurück.
+	 */
+	private ActionListener okListener(){
+		ActionListener ok = new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {	
+				//ändert die Funktionalität des Ok-Buttons je nach Aktion:
+				switch(aktion){
+					case 0:
+						registrieren();
+						break;
+					case 1:
+						adresseAendern();
+						break;
+					case 2:
+						mitarbeiterAnlegen();
+						break;
+				}
+			}
+		};
+		return ok;
+	}
+	
+	/**
+	 * Die Methode implementiert das Verhalten des Ok-Buttons für die Aktion 0.
+	 */
+	private void registrieren(){
+		//Deklariert und initialisiert alle einzulesenden Werte:
+		String vorName = vorNameText.getText();
+		String nachName = nachNameText.getText();
+		String kennwort = kennwortText.getText();
+		String strasse = strasseText.getText();
+		String plz = plzText.getText();
+		String stadt = stadtText.getText();
+		
+		//prüft, ob in jedem Feld ein Wert steht:
+		if(vorName.length() == 0 ||
+		   nachName.length() == 0 ||
+		   kennwort.length() == 0 ||
+		   strasse.length() == 0 ||
+		   plz.length() == 0 ||
+		   stadt.length() == 0){
+			JOptionPane dialog = new JOptionPane();
+			JOptionPane.showMessageDialog(RegistrierenFenster.this, "Sie müssen in jedes Feld einen Wert eintragen.", "Error", JOptionPane.ERROR_MESSAGE);
+			dialog.setVisible(true);
+			return;
+		}
+		
+		//holt sich eine neue Kunden-ID aus der EShopVerwaltung und legt ein neues Adress-Objekt an:
+		int neueKundenId = HauptFenster.shopVerwaltung.getNeueKundenId();
+		Adresse adresse = new Adresse(plz, stadt, strasse);
+		
+		//versucht einen neuen Kunden anzulegen und gibt ggf. eine Fehlermeldung aus:
+		try {
+			HauptFenster.eShopVerwaltung.kundeAnlegen(new Kunde(neueKundenId, vorName, nachName, kennwort, adresse));
+		} catch (BereitsVorhandenException e) {
+			JOptionPane dialog = new JOptionPane();
+			JOptionPane.showMessageDialog(RegistrierenFenster.this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			dialog.setVisible(true);
+			return;
+		}
 
+		//speichern:
+		try {
+			HauptFenster.shopVerwaltung.kundenSpeichern();
+		} catch (KonnteNichtSpeichernException e) {
+			JOptionPane dialog = new JOptionPane();
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			dialog.setVisible(true);
+		}
+		
+		//teilt dem Kunden bei Erfolg seine neue ID mit:
+		JOptionPane dialog = new JOptionPane();
+		JOptionPane.showMessageDialog(RegistrierenFenster.this, "Sie können sich nun mit der ID " + neueKundenId + " und dem von Ihnen gewählten Passwort anmelden.", "Kunde angelegt", JOptionPane.INFORMATION_MESSAGE);
+		dialog.setVisible(true);
+		this.dispose();
+	}
+	
+	/**
+	 * Die Methode implementiert das Verhalten des Ok-Buttons für die Aktion 1.
+	 */
+	private void adresseAendern(){
+		//deklariert und initialisiert alle einzulesenden Werte:
+		String strasse = strasseText.getText();
+		String plz = plzText.getText();
+		String stadt = stadtText.getText();
+		
+		//prüft, ob in jedem Feld ein Wert steht:
+		if(strasse.length() == 0 ||
+		   plz.length() == 0 ||
+		   stadt.length() == 0){
+			JOptionPane dialog = new JOptionPane();
+			JOptionPane.showMessageDialog(RegistrierenFenster.this, "Sie müssen in jedes Feld einen Wert eintragen.", "Error", JOptionPane.ERROR_MESSAGE);
+			dialog.setVisible(true);
+			return;
+		}
+		
+		//holt sich den Benutzer des Hauptfensters und legt ein neues Adress-Objekt an:
+		Kunde k = (Kunde)HauptFenster.benutzer;
+		Adresse adresse = new Adresse(plz, stadt, strasse);
+		
+		//gibt dem Kunden seine neue Adresse:
+		k.setAdresse(adresse);
+
+		//speichern:
+		try {
+			HauptFenster.shopVerwaltung.kundenSpeichern();
+		} catch (KonnteNichtSpeichernException e) {
+			JOptionPane dialog = new JOptionPane();
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			dialog.setVisible(true);
+		}
+		
+		//positive Rückmeldung für den Kunden:
+		JOptionPane dialog = new JOptionPane();
+		JOptionPane.showMessageDialog(RegistrierenFenster.this, "Ihre Adresse wurde erfolgreich geändert.", "Adresse geändert", JOptionPane.INFORMATION_MESSAGE);
+		dialog.setVisible(true);
+		this.dispose();
+	}
+	
+	/**
+	 * Die Methode implementiert das Verhalten des Ok-Buttons für die Aktion 2.
+	 */
+	private void mitarbeiterAnlegen(){
+		//deklariert und initialisiert alle einzulesenden Werte:
+		String vorName = vorNameText.getText();
+		String nachName = nachNameText.getText();
+		String kennwort = kennwortText.getText();
+		
+		//prüft, ob in jedem Feld ein Wert steht:
+		if(vorName.length() == 0 ||
+		   nachName.length() == 0 ||
+		   kennwort.length() == 0){
+			JOptionPane dialog = new JOptionPane();
+			JOptionPane.showMessageDialog(RegistrierenFenster.this, "Sie müssen in jedes Feld einen Wert eintragen.", "Error", JOptionPane.ERROR_MESSAGE);
+			dialog.setVisible(true);
+			return;
+		}
+		
+		//holt sich eine neue Mitarbeiter-ID aus der EShopVerwaltung:
+		int neueMitarbeiterId = HauptFenster.shopVerwaltung.getNeueMitarbeiterId();
+		
+		//versucht einen neuen Mitarbeiter anzulegen und gibt ggf. eine Fehlermeldung aus:
+		try {
+			HauptFenster.shopVerwaltung.mitarbeiterAnlegen(new Mitarbeiter(neueMitarbeiterId, vorName, nachName, kennwort));
+		} catch (BereitsVorhandenException e) {
+			JOptionPane dialog = new JOptionPane();
+			JOptionPane.showMessageDialog(RegistrierenFenster.this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			dialog.setVisible(true);
+			return;
+		}
+
+		//speichern:
+		try {
+			HauptFenster.shopVerwaltung.mitarbeiterSpeichern();
+		} catch (KonnteNichtSpeichernException e) {
+			JOptionPane dialog = new JOptionPane();
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			dialog.setVisible(true);
+		}
+		
+		//teilt dem Mitarbeiter die neue ID mit:
+		JOptionPane dialog = new JOptionPane();
+		JOptionPane.showMessageDialog(RegistrierenFenster.this, "Der Mitarbeiter kann sich nun mit der ID " + neueMitarbeiterId + " und dem von Ihnen gewählten Passwort anmelden.", "Mitarbeiter angelegt", JOptionPane.INFORMATION_MESSAGE);
+		dialog.setVisible(true);
+		this.dispose();
+	}
+	
+	/**
+	 * Die Methode gibt das RegistrierenPanel zurück.
+	 * @return Gibt das RegistrierenPanel zurück.
+	 */
+	public JPanel getRegistrierenPanel(){
+		return this.registrierenPanelErzeugen();
+	}
+	
+	/**
+	 * Die Methode gibt das AdressPanel zurück.
+	 * @return Gibt das AdressPanel zurück.
+	 */
+	public JPanel getAdressPanel(){
+		return this.adressPanelErzeugen();
+	}
+	
+	/**
+	 * Die Methode gibt das aktuelleAdressePanel zurück.
+	 * @return Gibt das aktuelleAdressePanel zurück.
+	 */
+	public JPanel getAktuelleAdressePanel(){
+		return this.aktuelleAdressePanelErzeugen();
+	}
 }
--- eShop2.0/src/GUI/WarenkorbFenster.java
+++ eShop2.0/src/GUI/WarenkorbFenster.java
@@ -1,10 +1,122 @@
 package GUI;
 
-import javax.swing.JFrame;
+import java.awt.GridLayout;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.NumberFormat;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import exceptions.BereitsVorhandenException;
+import Valueobjects.Artikel;
+import Valueobjects.Kunde;
+import Valueobjects.MehrfachArtikel;
+import Valueobjects.Position;
+
 
 @SuppressWarnings("serial")
-public class WarenkorbFenster extends JFrame{
-	public WarenkorbFenster() {
+public class WarenkorbFenster extends JDialog{
+	
+	private JFormattedTextField anzahlText;
+	private JButton abbrechenButton;
+	private JButton okButton;
+	private JPanel buttonPanel;
+	private JLabel textLabel;
+	private Artikel artikel;
+	
+	/**
+	 * Initialisiert ein modales WarenkorbFenster-Objekt mit übergebenem Hauptfenster und fester Größe.
+	 * @param owner Das Hauptfenster des Warenkorbfensters.
+	 */
+	public WarenkorbFenster(Window owner, Artikel a){
+		super(owner, "Artikel zum Warenkorb hinzufügen", ModalityType.APPLICATION_MODAL);
 		
+		this.artikel = a;
+		
+		this.setResizable(false);
+		this.initialisieren();
+	}
+	
+	/**
+	 * Die Methode stellt das Warenkorbfenster mit allen ActionListenern und Buttons zusammen.
+	 */
+	private void initialisieren(){
+		//setzt das Layout und initialisiert alle Komponenten des Warenkorbfensters:
+		this.setLayout(new GridLayout(3,1));
+		anzahlText = new JFormattedTextField(NumberFormat.getIntegerInstance());
+		okButton = new JButton("Ok");
+		abbrechenButton = new JButton("Abbrechen");
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(1,2));
+		textLabel = new JLabel("Sie haben folgenden Artikel gewählt: " + artikel.getArtikelName() + ". Wie viel möchten sie in den Warenkorb tun?");
+		final WarenkorbFenster gui = this;
+		
+		//erstellt einen ActionListener für den Abbrechen-Button und fügt die ActionListener den Buttons hinzu:
+		ActionListener abort = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				gui.dispose();
+			}
+		};
+		abbrechenButton.addActionListener(abort);
+		okButton.addActionListener(this.okButton());
+		
+		//fügt die Komponenten in das Layout ein:
+		this.add(textLabel);
+		this.add(anzahlText);
+		buttonPanel.add(okButton);
+		buttonPanel.add(abbrechenButton);
+		this.add(buttonPanel);
+		
+		//fügt alle Komponenten mit der optimale Größe zusammen
+		this.pack();
+	}
+	
+	/**
+	 * Die Methode implementiert den ActionListener für den Ok-Button und gibt diesen zurück.
+	 * @return Gibt den ActionListener für den Ok-Button zurück.
+	 */
+	private ActionListener okButton(){
+		//Selbstreferenz auf das Warenkorbfenster für die anonyme Klasse:
+		final WarenkorbFenster gui = this;
+		ActionListener ok = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//legt den Artikel in den Warenkorb und prüft auf Massengut:
+				boolean massengut = artikel instanceof MehrfachArtikel;
+				MehrfachArtikel tmp = massengut ? (MehrfachArtikel)artikel : null;
+				int einheit = massengut ? tmp.getVerkaufsEinheit() : 1;
+				try {
+					int menge = Integer.parseInt(gui.anzahlText.getText());
+					if(menge % einheit == 0){
+						Kunde k = (Kunde)HauptFenster.benutzer;
+						k.getWarenkorb().positionHinzufuegen(new Position(artikel, menge));
+					}else{
+						JOptionPane dialog = new JOptionPane();
+						JOptionPane.showMessageDialog(WarenkorbFenster.this, "Bitte geben Sie " + einheit + " oder ein Vielfaches von " + einheit + " an.", "Error", JOptionPane.ERROR_MESSAGE);
+						dialog.setVisible(true);
+						return;
+					}
+					JOptionPane dialog = new JOptionPane();
+					JOptionPane.showMessageDialog(WarenkorbFenster.this, "Folgender Artikel befindet sich jetzt in Ihrem Warenkorb: " + artikel.getArtikelName() + ".", "Artikel im Warenkorb", JOptionPane.INFORMATION_MESSAGE);
+					dialog.setVisible(true);
+					gui.dispose();
+				}catch (NumberFormatException e1) {
+					JOptionPane dialog = new JOptionPane();
+					JOptionPane.showMessageDialog(WarenkorbFenster.this, "Bitte geben Sie eine Zahl ein.", "Error", JOptionPane.ERROR_MESSAGE);
+					dialog.setVisible(true);
+				} catch (BereitsVorhandenException e1) {
+					JOptionPane dialog = new JOptionPane();
+					JOptionPane.showMessageDialog(WarenkorbFenster.this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					dialog.setVisible(true);
+					gui.dispose();
+				}
+			}
+		};
+		return ok;
 	}
 }
